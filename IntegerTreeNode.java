5a99a9ffb2fec57efb1966b709f2434edeca2ee7
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-27 14:30:05 +0000

    Exercise 1 completed

---
+++ IntegerTreeNode.java
@@ -0,0 +1,65 @@
+public class IntegerTreeNode {
+	private int value;
+	private IntegerTreeNode left;
+	private IntegerTreeNode right;
+
+	public IntegerTreeNode(int value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	public void add(int newNumber) {
+		if (newNumber > this.value) {
+			if (right == null) {
+				right = new IntegerTreeNode(newNumber);
+			} else {
+				right.add(newNumber);
+			}
+		} else {
+			if (left == null) {
+				left = new IntegerTreeNode(newNumber);
+			} else {
+				left.add(newNumber);
+			}
+		}
+	}
+
+	public boolean contains(int n) {
+		if (n == this.value) {
+			return true;
+		} else if (n > this.value) {
+			if (right == null) {
+				return false;
+			} else {
+				return right.contains(n);
+			}
+		} else {
+			if (left == null) {
+				return false;
+			} else {
+				return left.contains(n);
+			}
+		}
+	}
+
+	public int getMax() {
+		int max = this.value;
+		if (right == null) {
+			return max;
+		}else {
+			max = right.getMax();
+			return max;
+		}
+	}
+
+	public int getMin() {
+		int min = this.value;
+		if (left == null) {
+			return min;
+		} else {
+			min = left.getMin();
+			return min;
+		}
+	}
+}
