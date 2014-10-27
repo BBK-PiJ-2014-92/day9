17b1126882c06520d245f3416cb117ffba57e635
Ahmed Almazidi <a.almazidi92@gmail.com>
2014-10-27 15:10:25 +0000

    Finished ex 1.2

--- IntegerTreeNodeTest.java
+++ IntegerTreeNodeTest.java
@@ -1,21 +1,13 @@
 public class IntegerTreeNodeTest {
 
 	public static void main (String[] args) {
-		IntegerTreeNode tn = new IntegerTreeNode(5);
-		tn.add(6);
+		IntegerTreeNode tn = new IntegerTreeNode(6);
+		tn.add(5);
 		tn.add(9);
-		tn.add(4);
-		tn.add(1);
 		tn.add(3);
-		tn.add(10);
-		tn.add(2);
-		System.out.println(tn.contains(2));
-		System.out.println(tn.contains(6));
-		System.out.println(tn.contains(4));
-		System.out.println(tn.contains(5));
-		System.out.println(tn.contains(0));
-		System.out.println(tn.contains(10));
-		System.out.println(tn.getMax());
-		System.out.println(tn.getMin());
+		tn.add(8);
+		tn.add(11);
+		tn.add(12);
+		System.out.println(tn.toString());
 	}
 }
